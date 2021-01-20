9e9f2f8036f53cced1c34a51b51ad5180bce63cc
Fran <ffrannabril@gmail.com>
2021-01-20 16:44:43 +0100

    refactor: renamed project to agube-fe instead of project-completed

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/project-completed'),
+      dir: require('path').join(__dirname, './coverage/agube-fe'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-completed'`, () => {
+  it(`should have as title 'agube-fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-completed');
+    expect(app.title).toEqual('agube-fe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('project-completed app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('agube-fe app is running!');
   });
 });
