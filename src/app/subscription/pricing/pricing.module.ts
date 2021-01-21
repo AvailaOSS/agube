a19c4bbced151c5618465be3de775ba686739732
Fran <ffrannabril@gmail.com>
2021-01-21 10:28:51 +0100

    feat: npm i @angular/flex-layout & added MatCardModule

--- src/app/subscription/pricing/pricing.module.ts
+++ src/app/subscription/pricing/pricing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PricingComponent } from './pricing.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [PricingComponent],
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, MatCardModule, FlexLayoutModule],
   exports: [PricingComponent],
 })
 export class PricingModule {}
