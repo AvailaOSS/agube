a0837ce969a9fcda993e2d653244f45469290b85
Fran <ffrannabril@gmail.com>
2021-01-24 18:24:13 +0100

    feat: added client and president details card

--- src/app/control-panel/control-panel.component.ts
+++ src/app/control-panel/control-panel.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+export interface ClientDetails {
+  name: string;
+  nif: string;
+  address: string;
+  subscription_date: string;
+  plan: string;
+}
+
+export interface PresidentDetails {
+  name: string;
+  phone: string;
+  address: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-control-panel',
   templateUrl: './control-panel.component.html',
@@ -8,9 +23,25 @@ import { Router } from '@angular/router';
 })
 export class ControlPanelComponent implements OnInit {
   public users: any;
+  public clientDetails: ClientDetails;
+  public president: PresidentDetails;
+
   constructor(private readonly route: Router) {}
 
   public ngOnInit(): void {
+    this.clientDetails = {
+      name: 'Juego de Tronos',
+      nif: '999999999999999A',
+      address: 'Invernalia, 5, Piso 1',
+      subscription_date: 'Fecha Alta: 17 de abril de 2011',
+      plan: 'Expert',
+    };
+    this.president = {
+      name: 'John Snow',
+      phone: '656456459',
+      address: 'Invernalia, 5, Piso 1',
+      email: 'johnsnow69@gmail.com',
+    };
     this.users = JSON.parse(localStorage.getItem('users')) || [];
   }
 }
--- src/app/control-panel/control-panel.module.ts
+++ src/app/control-panel/control-panel.module.ts
@@ -5,6 +5,7 @@ import { MenuModule } from '../menu/menu.module';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [ControlPanelComponent],
@@ -14,6 +15,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatCardModule,
     MatButtonModule,
     FlexLayoutModule,
+    MatListModule,
   ],
   exports: [ControlPanelComponent],
 })
