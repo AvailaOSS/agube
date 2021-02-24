210a64bd081ae49aa80b2c6ffbdc39e815b6b33b
Fran <ffrannabril@gmail.com>
2021-02-24 17:47:43 +0100

    feat: user add address

--- login/urls.py
+++ login/urls.py
@@ -1,9 +1,10 @@
-from login.views import UserCustomDetailListView, UserDwellingDetailView, UserCreatePhoneView, UserUpdateDeletePhoneView
+from login.views import UserCreateAddressView, UserCustomDetailListView, UserDwellingDetailView, UserCreatePhoneView, UserUpdateDeletePhoneView
 from django.urls import path, include
 
 __url_user = [
     path('', UserCustomDetailListView.as_view()),
     path('/<int:pk>/dwelling', UserDwellingDetailView.as_view()),
+    path('/<int:pk>/address', UserCreateAddressView.as_view()),
     path('/<int:pk>/phone', UserCreatePhoneView.as_view()),
     path('/<int:pk>/phone/<int:phone_id>', UserUpdateDeletePhoneView.as_view()),
     # path('/<int:pk>/address', UserChangeAddressView.as_view()),
--- login/views.py
+++ login/views.py
@@ -1,5 +1,5 @@
 from dwelling.models import DwellingOwner, DwellingResident
-from address.models import UserFullAddress
+from address.models import Address, FullAddress, UserFullAddress
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from drf_yasg.utils import swagger_auto_schema
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.views import APIView
 
-from login.serializers import (UserCustomDetailSerializer,
+from login.serializers import (UserAddressUpdateSerializer, UserCustomDetailSerializer,
                                UserUpdatePhoneSerializer)
 
 TAG = 'user'
@@ -71,8 +71,10 @@ class UserDwellingDetailView(APIView):
         Return list of user Dwelling
         """
         attrList = []
-        attrList.extend(list(map(lambda x: x.dwelling, DwellingOwner.objects.filter(user__id=pk))))
-        attrList.extend(list(map(lambda x: x.dwelling, DwellingResident.objects.filter(user__id=pk))))
+        attrList.extend(
+            list(map(lambda x: x.dwelling, DwellingOwner.objects.filter(user__id=pk))))
+        attrList.extend(
+            list(map(lambda x: x.dwelling, DwellingResident.objects.filter(user__id=pk))))
 
         list_of_serialized = []
         for dwelling in attrList:
@@ -212,3 +214,99 @@ class UserUpdateDeletePhoneView(APIView):
         user_phone.delete()
         user_phone.phone.delete()
         return Response({'status': 'delete successfull!'})
+
+
+def update_all_user_full_address_to_not_main(user_id):
+    try:
+        user_address = UserFullAddress.objects.get(user__id=user_id, main=True)
+        user_address.main = False
+        user_address.save()
+    except ObjectDoesNotExist:
+        pass
+
+
+def get_all_user_full_address_serialized(user):
+    list_of_serialized = []
+    for address_iteration in UserFullAddress.objects.filter(user=user):
+        full_address = address_iteration.full_address
+        data =   {
+            "id": address_iteration.id,
+            "full_address": {
+                "address": {
+                    "id": full_address.address.id,
+                    "street": full_address.address.street,
+                    "is_external": full_address.address.is_external
+                },
+                "id": full_address.id,
+                "number": full_address.number,
+                "flat": full_address.flat,
+                "gate": full_address.gate,
+                "town": full_address.town,
+            },
+            "main": address_iteration.main
+        }
+        list_of_serialized.append(
+            UserAddressUpdateSerializer(data, many=False).data)
+
+    return Response(list_of_serialized)
+
+
+class UserCreateAddressView(APIView):
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(
+        request_body=UserAddressUpdateSerializer,
+        responses={200: UserAddressUpdateSerializer(many=True)},
+        tags=[TAG],
+    )
+    def post(self, request, pk):
+        """
+        Add new User Full Address
+        """
+        user = User.objects.get(id=pk)
+        # extract data
+        full_address = request.data.pop('full_address')
+        main = request.data.pop('main')
+        # if new is main change others as not main
+        if main:
+            update_all_user_full_address_to_not_main(pk)
+        # create a new full address
+        self.create_address(user, full_address, main)
+
+        return get_all_user_full_address_serialized(user)
+
+    @classmethod
+    def create_address(cls, user, validated_data, main):
+        # Extract Address data
+        address_data = validated_data.pop('address')
+        street = address_data.pop('street')
+        is_external = address_data.pop('is_external')
+
+        # Create Address
+        new_address = Address.objects.create(
+            street=street, is_external=is_external)
+
+        # Extract Full Address data
+        number = validated_data.pop('number')
+        flat = validated_data.pop('flat')
+        gate = validated_data.pop('gate')
+        town = validated_data.pop('town')
+
+        # Create Full Address
+        new_full_address = FullAddress.objects.create(
+            address=new_address, number=number, flat=flat, gate=gate, town=town)
+
+        # Create User Full Address
+        UserFullAddress.objects.create(
+            user=user, full_address=new_full_address, main=main)
+
+    @swagger_auto_schema(
+        responses={200: UserAddressUpdateSerializer(many=True)},
+        tags=[TAG],
+    )
+    def get(self, request, pk):
+        """
+        Return list of User Full Address
+        """
+        user = User.objects.get(id=pk)
+        return get_all_user_full_address_serialized(user)
