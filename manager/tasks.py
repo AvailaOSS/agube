67d4b114acceec62621095a762eae17c72f4fa3b
Fran <ffrannabril@gmail.com>
2021-03-23 09:16:11 +0100

    feat: manager is his own manager

--- manager/tasks.py
+++ manager/tasks.py
@@ -4,11 +4,14 @@ from celery import shared_task
 from celery.decorators import task
 from django.contrib.auth.models import User
 
-from manager.models import Manager
+from manager.models import Manager, Person
 
 
 @task(name="new_user_published")
 def new_user_published(data):
     # TODO: check json is valid
     json_response = json.loads(data)
-    Manager.objects.create(user=User.objects.get(id=json_response["id"]))
+    user = User.objects.get(id=json_response["id"])
+    manager = Manager.objects.create(user=user)
+    # Important: create Person after create User
+    Person.objects.create(manager=manager, user=user)
