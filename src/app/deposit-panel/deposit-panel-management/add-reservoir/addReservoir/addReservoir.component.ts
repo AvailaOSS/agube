6f5363979549b0d639a15fca471a9ccf372e4321
Germán <germangaristo1989@gmail.com>
2021-03-31 19:55:25 +0200

    feat: add reservoir

--- src/app/deposit-panel/deposit-panel-management/add-reservoir/addReservoir/addReservoir.component.ts
+++ src/app/deposit-panel/deposit-panel-management/add-reservoir/addReservoir/addReservoir.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ReservoirService } from 'apiaux/agube-rest-api-lib/src/public-api';
+import {
+  ManagerService,
+  ReservoirService,
+} from 'apiaux/agube-rest-api-lib/src/public-api';
 
 @Component({
   selector: '',
@@ -8,27 +11,47 @@ import { ReservoirService } from 'apiaux/agube-rest-api-lib/src/public-api';
   styleUrls: ['./addReservoir.component.scss'],
 })
 export class AddReservoirComponent implements OnInit {
-
   public error: boolean = true;
   public username: string;
+  public user_id: string;
   constructor(
     private router: Router,
-    private readonly svcCreateNewReservoir: ReservoirService
-  ) {}
+    private readonly svcCreateNewReservoir: ReservoirService,
+    private readonly svcManager: ManagerService
+  ) {
+    this.svcManager.getManagerByUser().subscribe((value) => {
+      this.user_id = value.user_id;
+    });
+  }
 
-
-  public ngOnInit(): void{ }
+  public ngOnInit(): void {}
   public sendForm(event: any): void {
-    console.log(event)
-  //  this.svcCreateNewReservoir.createReservoir({})
-  //     .subscribe(
-  //       (value) => {
-  //         this.router.navigate(['/viviendas']);
-  //       },
-  //       (error) => {
-  //         this.error = false;
-
-  //       }
-  //     );
+    console.log(event);
+    this.svcCreateNewReservoir
+      .createReservoir({
+        user_id: +this.user_id,
+        full_address: {
+          address: {
+            town: event.town,
+            street: event.address,
+            is_external: true,
+          },
+          number: event.number,
+          flat: event.flat,
+          gate: event.gate,
+        },
+        capacity: '200',
+        outlet_flow: '2',
+        inlet_flow: '21',
+        water_meter: { code: event.code },
+      })
+      .subscribe(
+        (value) => {
+          this.router.navigate(['/depositos']);
+        },
+        (error) => {
+          this.error = false;
+        }
+      );
   }
 }
