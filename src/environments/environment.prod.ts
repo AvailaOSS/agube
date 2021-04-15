451473188aeb01ea677e6cb082a3b3c2d2584036
Germán <germangaristo1989@gmail.com>
2021-04-15 22:36:02 +0200

    fix: change auth url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const environment = {
   agubeBackendUrl: 'http://localhost:8003/api/v1/agube',
   contactBookBackendUrl: 'http://localhost:8002/api/v1/contact-book',
   subscriptionBackendUrl: 'http://localhost:8001/api/v1/subscription',
-  authBackendUrl: 'http://localhost:8000/api/v1/auth',
+  authBackendUrl: 'http://localhost:8000/api/v1/',
 };
