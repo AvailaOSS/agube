756cc3f8e05f49f92fcdd3d38ad6730bf8c699de
Germán <germangaristo1989@gmail.com>
2021-05-14 19:39:28 +0200

    fix: 401 go to login

--- src/app/auth/login/helpers/error.interceptor.ts
+++ src/app/auth/login/helpers/error.interceptor.ts
@@ -21,7 +21,7 @@ export class ErrorInterceptor implements HttpInterceptor {
       catchError((err) => {
         if (err.status === 401) {
           // auto logout if 401 response returned from api
-          // this.accountService.logout();
+          this.accountService.logout();
         }
 
         return throwError(err);
