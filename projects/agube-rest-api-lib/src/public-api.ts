2439d4c3c90f8154c5edc36e641fbc44439cdbef
Fran <ffrannabril@gmail.com>
2021-05-15 17:02:47 +0200

    feat: added generated api

--- projects/agube-rest-api-lib/src/public-api.ts
+++ projects/agube-rest-api-lib/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of agube-rest-api-lib
  */
 
-export * from './lib/agube-rest-api-lib.service';
-export * from './lib/agube-rest-api-lib.component';
-export * from './lib/agube-rest-api-lib.module';
+export * from './lib/model/models';
+export * from './lib/service/api';
+export * from './lib/agube.api.module';
