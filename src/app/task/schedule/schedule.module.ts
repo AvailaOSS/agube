d9374ecf9fc025daa7b1ec4f70b22581c4b3458a
Germán <germangaristo1989@gmail.com>
2021-05-22 19:57:32 +0200

    fix: create forms schedule

--- src/app/task/schedule/schedule.module.ts
+++ src/app/task/schedule/schedule.module.ts
@@ -1,12 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { ComponentsModule } from '../../components/components.module';
 import { ScheduleComponent } from './schedule.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { TaskModule } from '../task.module';
 
 @NgModule({
   declarations: [ScheduleComponent],
-  imports: [CommonModule, ComponentsModule, NgbDatepickerModule, MatIconModule],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+    NgbDatepickerModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+  ],
 })
 export class ScheduleModule {}
