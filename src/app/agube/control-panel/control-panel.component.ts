0cec8366d963607149798ae72ce4d40d484fce8d
Germán <germangaristo1989@gmail.com>
2021-06-01 18:43:43 +0200

    fix: toolbar fixed

--- src/app/agube/control-panel/control-panel.component.ts
+++ src/app/agube/control-panel/control-panel.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AccountService } from '@availa/auth-fe';
-import { isNull } from 'lodash';
 import { ContactBookRoute } from '../../contact-book/contact-book-route';
 import { AgubeRoute } from '../agube-route';
 
@@ -11,26 +9,16 @@ import { AgubeRoute } from '../agube-route';
   styleUrls: ['./control-panel.component.scss'],
 })
 export class ControlPanelComponent implements OnInit {
-  public users: any;
   public dwelling: string = AgubeRoute.DWELLING;
   public dwellingConfig: string = AgubeRoute.CONFIG;
   public dwellingReservoir: string = AgubeRoute.RESERVOIR;
   public contactBook: string = ContactBookRoute.CONTACT_BOOK;
   public wip: string = AgubeRoute.WIP;
 
-  constructor(
-    private router: Router,
-    private readonly svcAccountService: AccountService
-  ) {
-    this.svcAccountService.getUser().subscribe((value) => {
-      if (isNull(value)) {
-        this.svcAccountService.logout();
-      }
-    });
-  }
+  constructor(private router: Router) {}
 
   public ngOnInit(): void {
-    this.users = this.svcAccountService.getUser();
+    //
   }
 
   public goTo(route: string): void {
