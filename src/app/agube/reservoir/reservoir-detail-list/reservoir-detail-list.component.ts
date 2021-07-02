6492480d41cfda5491c698ca8d3e8335508a9327
Germán <germangaristo1989@gmail.com>
2021-07-02 18:03:25 +0200

    feat: add scss , sidebar-component

--- src/app/agube/reservoir/reservoir-detail-list/reservoir-detail-list.component.ts
+++ src/app/agube/reservoir/reservoir-detail-list/reservoir-detail-list.component.ts
@@ -82,8 +82,10 @@ export class ReservoirDetailListComponent implements OnInit {
 
   public ngOnInit(): void {
     this.svcReservoirService.getReservoirs().subscribe((value) => {
-      this.valuesReservoir = Object.keys(value[0]);
-      this.datasource = new BehaviorSubject<any[]>(value);
+      if (value.length !== 0) {
+        this.valuesReservoir = Object.keys(value[0]);
+        this.datasource = new BehaviorSubject<any[]>(value);
+      }
     });
   }
 
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventEmitter } from '@angular/core';
+interface SelectedComponent {
+  path: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent implements OnInit {
+  @Output() public currentComponentName: EventEmitter<string> =
+    new EventEmitter();
+  public menuComponents: SelectedComponent[] = [];
+  public toogleVision: boolean = false;
+  @Input() user: any ;
+
+  constructor(private readonly route: Router) {
+    this.menuComponents = [
+      { path: 'viviendas', name: 'Viviendas' },
+      { path: 'wip', name: 'Lectura de Contador' },
+      { path: 'depositos', name: 'Depósito' },
+      { path: 'incidence', name: 'Incidencias' },
+      { path: 'config', name: 'Configutación General' },
+    ];
+  }
+
+  ngOnInit(): void {}
+
+  public selectedComponent(component: SelectedComponent): void {
+    this.route.navigate([component.path]);
+    this.currentComponentName.emit(component.name);
+  }
+
+  public toogleSidebar(): void {
+    console.log(this.toogleVision);
+    this.toogleVision = !this.toogleVision;
+  }
+}
