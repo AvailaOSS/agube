05291af7b9d06bc889d8b069d0b5e15a8042340f
Fran <ffrannabril@gmail.com>
2021-07-20 17:16:34 +0200

    feat: add DJANGO_SECRET_KEY from environments

--- agube/settings.py
+++ agube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
