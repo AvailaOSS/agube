508d4265f5346af33ae0dfca04670c67ab7464b0
Germán <germangaristo1989@gmail.com>
2021-07-30 03:53:45 +0200

    FIX: update form update owner

--- src/app/agube/dwelling/dwelling-detail-card/management-detail-card/management-components/change-owner/change-owner.module.ts
+++ src/app/agube/dwelling/dwelling-detail-card/management-detail-card/management-components/change-owner/change-owner.module.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { DwellingUtilsModule } from '../dwelling-utils/dwelling-utils.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NotificationModule } from '@availa/notification';
 import { ChangeOwnerComponent } from './change-owner.component';
 
 @NgModule({
   declarations: [ChangeOwnerComponent],
-  imports: [CommonModule, DwellingUtilsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    FormsModule,
+    NotificationModule,
+  ],
 })
 export class ChangeOwnerModule {}
