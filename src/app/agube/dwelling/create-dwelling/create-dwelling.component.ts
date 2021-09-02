008c76a27b04ad62bbc97f8b3ffc98c57108bdf6
Fran <ffrannabril@gmail.com>
2021-09-02 20:51:28 +0200

    feat: create dwelling with the new post

--- src/app/agube/dwelling/create-dwelling/create-dwelling.component.ts
+++ src/app/agube/dwelling/create-dwelling/create-dwelling.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { DwellingService } from "@availa/agube-rest-api";
 import { NotificationService } from "@availa/notification";
@@ -10,87 +10,48 @@ import { Router } from "@angular/router";
   templateUrl: "./create-dwelling.component.html",
   styleUrls: ["./create-dwelling.component.scss"],
 })
-export class CreateDwellingComponent implements OnInit {
-  public createFormGroup: FormGroup;
-  public ownerFormGroup: FormGroup;
-  public residentFormGroup: FormGroup;
-  public paymasterFormGroup: FormGroup;
-  public radioGroupForm: FormGroup;
-
-  public submitted = false;
-  public options = {
-    autoClose: false,
-    keepAfterRouteChange: false,
-  };
+export class CreateDwellingComponent {
+  title = "Alta vivienda";
+  dwellingForm: FormGroup;
 
   constructor(
-    private readonly svcCreateNewDWelling: DwellingService,
-    public alertService: NotificationService,
-    private readonly svcRouter: Router,
-    private formBuilder: FormBuilder
-  ) {}
+    private formBuilder: FormBuilder,
+    private readonly router: Router,
+    public readonly svcAlert: NotificationService,
+    private readonly svcCreateNewDWelling: DwellingService
+  ) {
+    this.createForm();
+  }
 
-  public ngOnInit(): void {
-    this.createFormGroup = this.formBuilder.group({
-      street: ["", Validators.required],
-      number: ["", Validators.required],
-      gate: ["", Validators.required],
-      flat: ["", Validators.required],
-      city: ["", Validators.required],
-      waterMeter: ["", Validators.required],
-    });
-    this.ownerFormGroup = this.formBuilder.group({
-      name: ["", Validators.required],
-      lastName: ["", Validators.required],
-      username: ["", Validators.required],
-      email: ["", Validators.required],
-      phone: ["", Validators.required],
-      address: ["", Validators.required],
-    });
-    this.residentFormGroup = this.formBuilder.group({
-      nameRes: ["", Validators.required],
-      lastNameRes: ["", Validators.required],
-      usernameRes: ["", Validators.required],
-      emailRes: ["", Validators.required],
-      phoneRes: ["", Validators.required],
-    });
-    this.paymasterFormGroup = this.formBuilder.group({
-      iban: ["", Validators.required],
-    });
-    this.radioGroupForm = this.formBuilder.group({
-      model: 1,
+  private createForm() {
+    this.dwellingForm = this.formBuilder.group({
+      full_address: this.formBuilder.group({
+        address: this.formBuilder.group({
+          town: ["", Validators.required],
+          street: ["", Validators.required],
+          is_external: [false, Validators.required],
+        }),
+        number: ["", Validators.required],
+        flat: [],
+        gate: [],
+      }),
+      water_meter: this.formBuilder.group({
+        code: ["", Validators.required],
+      }),
     });
   }
 
-  get f() {
-    return this.createFormGroup.controls;
-  }
-
-  // FIXME: CAMBIAR IBAN EN EL FUTURO --- POSIBLE CAMBIO DE API
-  public onSubmit(): void {
-    this.svcCreateNewDWelling
-      .createDwelling({
-        full_address: {
-          address: {
-            town: this.createFormGroup.value.city,
-            street: this.createFormGroup.value.street,
-            is_external: true,
-          },
-          number: this.createFormGroup.value.number,
-          flat: this.createFormGroup.value.flat,
-          gate: this.createFormGroup.value.gate,
-        },
-        water_meter: {
-          code: this.createFormGroup.value.waterMeter,
-        },
-      })
-      .subscribe(
-        () => {
-          this.svcRouter.navigate([AgubeRoute.DWELLING]);
-        },
-        (err) => {
-          this.alertService.error(JSON.stringify(err.error), this.options);
-        }
-      );
+  onSubmit() {
+    this.svcCreateNewDWelling.createDwelling(this.dwellingForm.value).subscribe(
+      () => {
+        this.router.navigate([AgubeRoute.DWELLING]);
+      },
+      (err) => {
+        this.svcAlert.error(JSON.stringify(err.error), {
+          autoClose: false,
+          keepAfterRouteChange: false,
+        });
+      }
+    );
   }
 }
