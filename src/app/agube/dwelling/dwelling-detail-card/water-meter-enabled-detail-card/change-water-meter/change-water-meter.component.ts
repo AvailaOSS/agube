578b02e38921f164fb911846d65d2ff4e01c5d30
Fran <ffrannabril@gmail.com>
2021-09-09 17:58:25 +0200

    feat: change water meter code improved

--- src/app/agube/dwelling/dwelling-detail-card/water-meter-enabled-detail-card/change-water-meter/change-water-meter.component.ts
+++ src/app/agube/dwelling/dwelling-detail-card/water-meter-enabled-detail-card/change-water-meter/change-water-meter.component.ts
@@ -1,65 +1,50 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { DwellingService, WaterMeter } from '@availa/agube-rest-api';
-import { NotificationService } from '@availa/notification';
-import { AgubeRoute } from 'src/app/agube/agube-route';
+import { Component, Input, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { DwellingService, WaterMeter } from "@availa/agube-rest-api";
+import { NotificationService } from "@availa/notification";
+import { AgubeRoute } from "src/app/agube/agube-route";
 
 @Component({
-  selector: 'app-change-water-meter',
-  templateUrl: './change-water-meter.component.html',
-  styleUrls: ['./change-water-meter.component.scss'],
+  selector: "app-change-water-meter",
+  templateUrl: "./change-water-meter.component.html",
+  styleUrls: ["./change-water-meter.component.scss"],
 })
 export class ChangeWaterMeterComponent implements OnInit {
-  @Input() titleFormWaterMeter?: string = 'Cambio de Contador';
-  public waterMeterFormGroup: FormGroup;
-  public submitted = false;
-  public options = {
-    autoClose: true,
-    keepAfterRouteChange: false,
-  };
-  public waterMeterId: string;
-  public waterMeter: WaterMeter;
-  public waterMeterCode: string;
+  @Input() title?: string = "Cambio de Contador";
+  public waterMeterForm: FormGroup;
+  public id: number;
+
   constructor(
+    private formBuilder: FormBuilder,
     private readonly route: ActivatedRoute,
     private readonly svcChangeWaterMeter: DwellingService,
     private readonly alertService: NotificationService,
-    private readonly svcRouter: Router,
-    private formBuilder: FormBuilder
+    private readonly router: Router
   ) {
     this.route.queryParams.subscribe((params) => {
-      this.waterMeterId = params.data;
+      this.id = +params.data;
     });
-
-    this.svcChangeWaterMeter
-      .getCurrentDwellingWaterMeter(+this.waterMeterId)
-      .subscribe((value) => {
-        this.waterMeter = value;
-        this.waterMeterCode = value.code;
-      });
+    this.waterMeterForm = this.formBuilder.group({
+      code: ["", Validators.required],
+    });
   }
 
   public ngOnInit(): void {
-    this.waterMeterFormGroup = this.formBuilder.group({
-      waterMeter: ['', Validators.required],
-    });
+    this.svcChangeWaterMeter
+      .getCurrentDwellingWaterMeter(this.id)
+      .subscribe((response) =>
+        this.waterMeterForm.get("code").setValue(response.code)
+      );
   }
-  get f() {
-    return this.waterMeterFormGroup.controls;
-  }
+
   public onSubmit(): void {
     this.svcChangeWaterMeter
-      .changeCurrentDwellingWaterMeter(+this.waterMeterId, {
-        code: this.waterMeterFormGroup.value.waterMeter,
-      })
+      .changeCurrentDwellingWaterMeter(this.id, this.waterMeterForm.value)
       .subscribe(
-        (value) => {
-          this.svcRouter.navigate([AgubeRoute.DWELLING]);
-        },
-        (error) => {
-          this.alertService.error('Error al actualizad ' + error.error.status);
-        }
+        (value) => this.router.navigate([AgubeRoute.DWELLING]),
+        (error) =>
+          this.alertService.error("Error al actualizar " + error.error.status)
       );
   }
 }
