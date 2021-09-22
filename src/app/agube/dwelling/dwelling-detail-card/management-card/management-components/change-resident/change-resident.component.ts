31aa6ef747eea532d11efb45a953e7ad0effcf28
Germán <germangaristo1989@gmail.com>
2021-09-22 20:15:23 +0200

    feat : new form to add phone and address

--- src/app/agube/dwelling/dwelling-detail-card/management-card/management-components/change-resident/change-resident.component.ts
+++ src/app/agube/dwelling/dwelling-detail-card/management-card/management-components/change-resident/change-resident.component.ts
@@ -1,20 +1,27 @@
-import { Component, Input } from "@angular/core";
-import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { ActivatedRoute, Router } from "@angular/router";
-import { DwellingService } from "@availa/agube-rest-api";
-import { NotificationService } from "@availa/notification";
-import { AgubeRoute } from "../../../../../agube-route";
+import { Component, Input } from '@angular/core';
+import {
+  Form,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Address, DwellingService } from '@availa/agube-rest-api';
+import { NotificationService } from '@availa/notification';
+import { AgubeRoute } from '../../../../../agube-route';
 
 @Component({
-  selector: "app-change-resident",
-  templateUrl: "./change-resident.component.html",
-  styleUrls: ["./change-resident.component.scss"],
+  selector: 'app-change-resident',
+  templateUrl: './change-resident.component.html',
+  styleUrls: ['./change-resident.component.scss'],
 })
 export class ChangeResidentComponent {
-  title = "Alta Residente";
+  title = 'Alta Residente';
   dwellingId: number;
   residentForm: FormGroup;
-  userForm: FormGroup;
+  userForm: FormArray;
+  public addressComplement: any;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -29,81 +36,68 @@ export class ChangeResidentComponent {
     });
   }
 
-  private createForm() {
-    // {
-    //   "user": {
-    //     "first_name": "string",
-    //     "last_name": "string",
-    //     "email": "user@example.com",
-    //     "phones": [
-    //       {
-    //         "phone_number": "string"
-    //       }
-    //     ],
-    //     "address": [
-    //       {
-    //         "address": {
-    //           "town": "string",
-    //           "street": "string",
-    //           "is_external": true
-    //         },
-    //         "number": 0,
-    //         "flat": "string",
-    //         "gate": "string"
-    //       }
-    //     ]
-    //   }
-    // }
+  private createForm(): void {
     this.residentForm = this.formBuilder.group({
-      user: this.formBuilder.group({
-        first_name: ["", Validators.required],
-        last_name: ["", Validators.required],
-        email: ["", Validators.required],
-        phones: this.formBuilder.array([]),
-        address: this.formBuilder.array([]),
-      }),
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      address: this.formBuilder.array([this.addAddress()]),
+      phones: this.formBuilder.array([this.addPhone()]),
     });
-    this.userForm = this.residentForm.controls.user as FormGroup;
   }
 
-  public addPhones() {
-    const phones = this.userForm.controls.phones as FormArray;
-    phones.push(
-      this.formBuilder.group({
-        phone_number: ["", Validators.required],
-      })
-    );
+  public addPhone(): FormGroup {
+    return this.formBuilder.group({
+      phone_number: ['', Validators.required],
+    });
+  }
+  public addPhoneButton(): void {
+    this.userForm = this.residentForm.get('phones') as FormArray;
+    this.userForm.push(this.addPhone());
+  }
+  public removePhone(index: number): void {
+    this.userForm = this.residentForm.get('phones') as FormArray;
+    this.userForm.removeAt(index);
   }
 
-  public removePhone(index) {
-    const phones = this.userForm.controls.phones as FormArray;
-    phones.removeAt(index);
+  public addAddress(): FormGroup {
+    return this.formBuilder.group({
+      number: ['', Validators.required],
+      flat: ['', Validators.required],
+      gate: ['', Validators.required],
+      town: ['', Validators.required],
+      street: ['', Validators.required],
+      is_external: [false, Validators.required],
+    });
   }
 
-  public addAddress() {
-    const address = this.userForm.controls.address as FormArray;
-    address.push(
-      this.formBuilder.group({
-        number: ["", Validators.required],
-        flat: ["", Validators.required],
-        gate: ["", Validators.required],
-        address: this.formBuilder.group({
-          town: ["", Validators.required],
-          street: ["", Validators.required],
-          is_external: [false, Validators.required],
-        }),
-      })
-    );
+  public addAddressButton(): void {
+    this.userForm = this.residentForm.get('address') as FormArray;
+    this.userForm.push(this.addAddress());
   }
-
-  public removeAddress(index) {
-    const phones = this.userForm.controls.phones as FormArray;
-    phones.removeAt(index);
+  public removeAddress(index: number): void {
+    this.userForm = this.residentForm.get('address') as FormArray;
+    this.userForm.removeAt(index);
   }
 
   public onSubmit(): void {
+    this.residentForm.value.address.forEach((value) => {
+      this.addressComplement = {
+        flat: value.flat,
+        gate: value.gate,
+        address: {
+          town: value.town,
+          street: value.street,
+          is_external: value.is_external,
+        },
+      };
+    });
+
+    this.residentForm.value.address = this.addressComplement;
     this.svcCreateNewDWelling
-      .changeCurrentResident(this.dwellingId, this.residentForm.value)
+      .changeCurrentResident(this.dwellingId, {
+        user: this.residentForm.value,
+      })
       .subscribe(
         () => {
           this.router.navigate([AgubeRoute.DWELLING]);
