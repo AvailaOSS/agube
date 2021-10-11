22ecce529b48f4ea3aecad637fdc5127a742e064
Fran <ffrannabril@gmail.com>
2021-10-11 21:14:13 +0200

    feat: show reservoir info

--- src/app/agube/reservoir/reservoir-detail-card/management-card/reservoir-management-card.component.ts
+++ src/app/agube/reservoir/reservoir-detail-card/management-card/reservoir-management-card.component.ts
@@ -1,40 +1,18 @@
-import { Component, Input, OnChanges, OnInit } from "@angular/core";
-import { ReservoirCreate, ReservoirService } from "@availa/agube-rest-api";
-import { isUndefined } from "lodash";
+import { Component, Input, OnInit } from '@angular/core';
+import { ReservoirCreate } from '@availa/agube-rest-api';
 
 @Component({
-  selector: "app-reservoir-management-card",
-  templateUrl: "./reservoir-management-card.component.html",
+  selector: 'app-reservoir-management-card',
+  templateUrl: './reservoir-management-card.component.html',
 })
-export class ReservoirManagementComponent implements OnInit, OnChanges {
-  @Input() reservoirId: number;
-  public reservoir: ReservoirCreate;
+export class ReservoirManagementComponent implements OnInit {
+  @Input() reservoir: ReservoirCreate;
 
-  constructor(private readonly svcReservoir: ReservoirService) {
-    this.reservoir = {
-      full_address: {
-        address: { street: "", town: "" },
-        number: 0,
-      },
-      user_id: 0,
-      water_meter: { code: "", discharge_date: "" },
-      capacity: "",
-      inlet_flow: "",
-      outlet_flow: "",
-    };
+  constructor() {
+    //
   }
 
   public ngOnInit(): void {
-    if (!isUndefined(this.reservoirId)) {
-      this.svcReservoir.getReservoir(this.reservoirId).subscribe((result) => {
-        if (!isUndefined(result)) {
-          this.reservoir = result;
-        }
-      });
-    }
-  }
-
-  public ngOnChanges(): void {
-    this.ngOnInit();
+    //
   }
 }
