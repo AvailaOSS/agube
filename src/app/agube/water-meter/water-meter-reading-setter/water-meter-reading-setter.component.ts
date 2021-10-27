d7894c7ba7307f5130d6d5245a90d99250aa7621
Germán <germangaristo1989@gmail.com>
2021-10-28 00:35:22 +0200

    fix: dialog waterMeter

--- src/app/agube/water-meter/water-meter-reading-setter/water-meter-reading-setter.component.ts
+++ src/app/agube/water-meter/water-meter-reading-setter/water-meter-reading-setter.component.ts
@@ -1,17 +1,68 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { WaterMeterService } from "@availa/agube-rest-api";
-import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
-import * as moment from "moment";
-
+import {
+  Component,
+  Input,
+  OnInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { WaterMeterService } from '@availa/agube-rest-api';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { formatISO, formatISO9075 } from 'date-fns';
+import * as moment from 'moment';
 @Component({
-  selector: "app-water-meter-reading-setter",
-  templateUrl: "./water-meter-reading-setter.component.html",
-  styleUrls: ["./water-meter-reading-setter.component.scss"],
+  selector: 'app-water-meter-reading-setter',
+  templateUrl: './water-meter-reading-setter.component.html',
+  styleUrls: ['./water-meter-reading-setter.component.scss'],
 })
 export class WaterMeterReadingSetterComponent implements OnInit {
   @Input() public id: any;
-  public readingForm: FormGroup;
+  public readingForm: FormGroup = new FormGroup({});
+  model = {
+    date: formatISO(new Date(), { representation: 'date' }),
+    time: formatISO9075(new Date(), { representation: 'time' }),
+    measures: 1,
+  };
+  public fields: FormlyFieldConfig[] = [
+    {
+      validators: {
+        validation: [
+          { name: 'fieldMatch', options: { errorPath: 'date' } },
+          { name: 'fieldMatch', options: { errorPath: 'time' } },
+        ],
+      },
+      fieldGroup: [
+        {
+          key: 'date',
+          type: 'input',
+          templateOptions: {
+            type: 'date',
+            label: 'Fecha',
+            required: true,
+          },
+        },
+        {
+          key: 'time',
+          type: 'input',
+          templateOptions: {
+            type: 'time',
+            label: 'Hora',
+            required: true,
+          },
+        },
+        {
+          key: 'measurement',
+          type: 'input',
+          templateOptions: {
+            type: 'input',
+            label: 'Lectura',
+            placeholder: 'Lectura',
+            required: true,
+          },
+        },
+      ],
+    },
+  ];
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -21,29 +72,25 @@ export class WaterMeterReadingSetterComponent implements OnInit {
     //
   }
 
-  ngOnInit(): void {
-    var now = moment();
-    this.readingForm = this.formBuilder.group({
-      date: [now.format("YYYY-MM-DD"), Validators.required],
-      time: [now.format("HH:mm"), Validators.required],
-      measurement: ["", Validators.required],
-    });
+  public onSubmit(model: any): void {
+    const dateModel = new Date(
+      model.date.split('-')[0],
+      model.date.split('-')[1] - 1,
+      model.date.split('-')[2],
+      model.time.split(':')[0],
+      model.time.split(':')[1]
+    );
+    this.svcWaterMeter
+      .addWaterMeterMeasure(this.id, {
+        measurement: model.measurement,
+        date: dateModel,
+      })
+      .subscribe((response) => this.activeModal.close(response));
   }
+
+  public ngOnInit(): void {}
 
   public cancel(): void {
     this.activeModal.close();
-  }
-
-  public save(): void {
-    const time = moment(this.readingForm.value.time, "HH:mm");
-    this.svcWaterMeter
-      .addWaterMeterMeasure(this.id, {
-        measurement: this.readingForm.value.measurement,
-        date: moment(this.readingForm.value.date)
-          .add(time.hours(), "hours")
-          .add(time.minutes(), "minutes")
-          .toDate(),
-      })
-      .subscribe((response) => this.activeModal.close(response));
   }
 }
--- src/app/agube/water-meter/water-meter.module.ts
+++ src/app/agube/water-meter/water-meter.module.ts
@@ -1,12 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { NotificationModule } from '@availa/notification';
 import { TableModule } from '@availa/table';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyModule } from '@ngx-formly/core';
+import { isUndefined } from 'lodash';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { ChangeWaterMeterComponent } from './change-water-meter/change-water-meter.component';
 import { WaterMeterReadingsComponent } from './water-meter-reading-detail/water-meter-reading-detail-card.component';
 import { WaterMeterReadingSetterComponent } from './water-meter-reading-setter/water-meter-reading-setter.component';
+
+export function fieldMatchValidator(control: AbstractControl): any {
+  if (!isUndefined(control.value.date) && !isUndefined(control.value.time)) {
+    const dateModel = new Date(
+      control.value.date.split('-')[0],
+      control.value.date.split('-')[1] - 1,
+      control.value.date.split('-')[2],
+      control.value.time.split(':')[0],
+      control.value.time.split(':')[1]
+    );
+
+    if (dateModel > new Date()) {
+      return { fieldMatch: { message: 'No se puede agregar datos a futuro' } };
+    }
+  } else {
+    return null;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -21,6 +46,13 @@ import { WaterMeterReadingSetterComponent } from './water-meter-reading-setter/w
     TableModule,
     NotificationModule,
     ComponentsModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({
+      validators: [{ name: 'fieldMatch', validation: fieldMatchValidator }],
+      validationMessages: [
+        { name: 'required', message: 'Este campo es obligatorio' },
+      ],
+    }),
   ],
   exports: [
     WaterMeterReadingsComponent,
