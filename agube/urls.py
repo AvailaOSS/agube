815c784d678229cc5929bdc4148d4c9088f9ed2e
Santi <sbl93.it@gmail.com>
2021-10-29 14:06:22 +0200

    feat: swagger auth and root redirection

--- agube/urls.py
+++ agube/urls.py
@@ -2,6 +2,7 @@ from address.urls import urlpatterns as urls_address
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from dwelling.urls import urlpatterns as urls_dwelling
@@ -41,6 +42,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns.append(path('admin/', admin.site.urls))
+    urlpatterns.append(path('api-auth/', include('rest_framework.urls')))
     urlpatterns.append(
         re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'))
     urlpatterns.append(re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'))
+    urlpatterns.append(path('', RedirectView.as_view(url='swagger', permanent=False), name='api-root'))
