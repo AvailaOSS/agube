2d42a0cead941a4751af392e291b58aa190b89c1
Santi <sbl93.it@gmail.com>
2021-12-07 15:01:08 +0100

    feat: user main address 1st in lists

--- login/assemblers.py
+++ login/assemblers.py
@@ -1,15 +1,24 @@
 from django.contrib.auth.models import User
 
 from login.models import UserAddress, UserPhone
-
+from login.serializers import UserAddressUpdateSerializer
 
 def get_all_user_full_address_serialized(user: User):
-    from login.serializers import UserAddressUpdateSerializer
+    list_of_serialized: list[UserAddressUpdateSerializer] = []
+    # Main address in 1st place
+    list_of_serialized = __serialize_user_addresses(UserAddress.objects.filter(user=user, main=True))
+    # Other addresses
+    alternative_address_list = UserAddress.objects.filter(user=user, main=False)
+    if alternative_address_list:
+        list_of_serialized.append(__serialize_user_addresses(alternative_address_list))
 
+    return list_of_serialized
+
+def __serialize_user_addresses(user_address_list):
     list_of_serialized: list[UserAddressUpdateSerializer] = []
 
     address_iteration: UserAddress
-    for address_iteration in UserAddress.objects.filter(user=user):
+    for address_iteration in user_address_list:
         full_address = address_iteration.full_address
         data = {
             "id": address_iteration.id,
@@ -31,7 +40,6 @@ def get_all_user_full_address_serialized(user: User):
             UserAddressUpdateSerializer(data, many=False).data)
 
     return list_of_serialized
-
 
 def get_all_user_phones_serialized(user: User):
     from login.serializers import UserPhoneUpdateSerializer
