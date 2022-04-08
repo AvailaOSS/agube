1396c3c7951d4c97d49921451d4f66bb24a83d1c
Fran <ffrannabril@gmail.com>
2022-04-08 10:21:40 +0200

    feat: manager config translate

--- src/app/config/manager-config/manager-config.component.ts
+++ src/app/config/manager-config/manager-config.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manager-config',
   templateUrl: './manager-config.component.html',
-  styleUrls: ['./manager-config.component.scss']
+  styleUrls: ['./manager-config.component.scss'],
 })
 export class ManagerConfigComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
