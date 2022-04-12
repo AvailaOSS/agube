c2bb4b821924f6605262a3b3ed62eef79bbac040
@germangaristo1989 <germangaristo1989@gmail.com>
2022-04-12 19:27:21 +0200

    feat: update auth-fe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AuthModule, AuthRoute } from '@availa/auth-fe';
+import { AuthModule, AuthRoute, HttpLoaderFactory } from '@availa/auth-fe';
+
 import { SubscriptionModule, SubscriptionRoute } from '@availa/subscription-fe';
 import { SidebarRoute } from './page/home/sidebar-route';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AgubeApiModule } from '@availa/agube-rest-api';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -41,13 +42,14 @@ import { MatTooltipModule } from '@angular/material/tooltip';
       basePath: environment.agubeBackendUrl,
     }),
     TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (http: HttpClient) => {
-          return new TranslateHttpLoader(http);
+      loader: [
+        {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
         },
-        deps: [HttpClient],
-      },
+      ],
+      isolate: true,
     }),
     MatMenuModule,
     MatButtonModule,
