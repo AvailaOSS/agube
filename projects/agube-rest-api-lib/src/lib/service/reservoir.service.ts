a48d5a50a7c0d1952447b4ef892b73fe004f2ae6
@germangaristo1989 <germangaristo1989@gmail.com>
2022-04-18 09:43:35 +0200

    feat: implemented reservoir geolocation

--- projects/agube-rest-api-lib/src/lib/service/reservoir.service.ts
+++ projects/agube-rest-api-lib/src/lib/service/reservoir.service.ts
@@ -21,6 +21,7 @@ import { Injectable, Optional } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Configuration } from '../configuration';
 import { AgubeRestConfigurationService } from '../configuration.service';
+import { Geolocation } from '../model/geolocation';
 import { Owner } from '../model/owner';
 import { ReservoirCreate } from '../model/reservoirCreate';
 import { ReservoirDetail } from '../model/reservoirDetail';
@@ -557,6 +558,53 @@ export class ReservoirService {
       }
     );
   }
+   /**
+     * 
+     * Return the Reservoir geolocation.
+     * @param id 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public getReservoirGeolocation(id: string, observe?: 'body', reportProgress?: boolean): Observable<Geolocation>;
+    public getReservoirGeolocation(id: string, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<Geolocation>>;
+    public getReservoirGeolocation(id: string, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<Geolocation>>;
+    public getReservoirGeolocation(id: string, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        if (id === null || id === undefined) {
+            throw new Error('Required parameter id was null or undefined when calling getReservoirGeolocation.');
+        }
+
+        let headers = this.defaultHeaders;
+
+        // authentication (Basic) required
+        if (this.configuration.username || this.configuration.password) {
+            headers = headers.set('Authorization', 'Basic ' + btoa(this.configuration.username + ':' + this.configuration.password));
+        }
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json'
+        ];
+
+        return this.httpClient.get<Geolocation>(`${this.basePath}/reservoir/${encodeURIComponent(String(id))}/geolocation`,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
 
   /**
    *
