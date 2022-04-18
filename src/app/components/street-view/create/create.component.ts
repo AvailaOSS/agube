b57971ad4099c9070ff4e4b8aaaff5fe79923785
@germangaristo1989 <germangaristo1989@gmail.com>
2022-04-18 23:28:02 +0200

    feat: added new tiles and change hover mat-list

--- src/app/components/street-view/create/create.component.ts
+++ src/app/components/street-view/create/create.component.ts
@@ -1,9 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostBinding,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { FullAddress } from '@availa/agube-rest-api';
 import { Observable } from 'rxjs';
 import { FullAddressPipe } from 'src/app/pipes/full-address.pipe';
 import * as L from 'leaflet';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 interface GeocoderResponse {
   status: string;
@@ -19,7 +26,7 @@ interface GeocoderResponse {
 export class CreateComponent implements AfterViewInit {
   @Input() public id: number | undefined;
   @Input() public address: FullAddress | undefined;
-
+  @HostBinding('class') className: any;
   private map: any;
 
   public selectedStreetCandidate: any;
@@ -29,7 +36,11 @@ export class CreateComponent implements AfterViewInit {
   private static mapURL: string = `https://nominatim.openstreetmap.org/search.php?q=`;
   private static sufixMapURL: string = `&polygon_geojson=1&limit=7&format=jsonv2`;
 
-  constructor(private http: HttpClient, private pipeAddress: FullAddressPipe) {}
+  constructor(
+    private http: HttpClient,
+    private pipeAddress: FullAddressPipe,
+    protected overlayContainer: OverlayContainer
+  ) {}
 
   ngAfterViewInit(): void {
     this.getLocation(this.address!).subscribe((response: any) => {
@@ -40,6 +51,8 @@ export class CreateComponent implements AfterViewInit {
         this.selectedStreetCandidate.lon
       );
     });
+
+    console.log(this.className);
   }
 
   public selectCandidate(candidate: any) {
@@ -81,13 +94,26 @@ export class CreateComponent implements AfterViewInit {
     });
 
     const tiles = L.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      // 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', //Hybrid
       { maxZoom: 19, minZoom: 13 }
     );
 
     tiles.addTo(this.map);
 
+    //DARK MODEs
+    // const Stadia_AlidadeSmoothDark = L.tileLayer(
+    //   'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+    //   {
+    //     maxZoom: 20,
+    //     attribution:
+    //       '© <a href="https://stadiamaps.com/">Stadia Maps</a>, © <a href="https://openmaptiles.org/">OpenMapTiles</a> © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    //   }
+    // );
+    // Stadia_AlidadeSmoothDark.addTo(this.map);
+
     L.circle([lat, lon], {
+      fillColor: '#7fd3f7',
       fillOpacity: 0.5,
       radius: 10,
     }).addTo(this.map);
@@ -96,6 +122,7 @@ export class CreateComponent implements AfterViewInit {
       let lat = e.latlng.lat;
       let lng = e.latlng.lng;
       this.initializeMap(lat, lng);
+
       this.getLocationByCoordinate(lat, lng).subscribe(
         (response) => (this.selectedStreetCandidate = response)
       );
