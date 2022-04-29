81e43776b9bdc45f0d010472b9ecf8f1455c0336
Fran <ffrannabril@gmail.com>
2022-04-29 15:20:06 +0200

    feat: only manager can add new reading

--- src/app/page/dwelling/detail/management/management.component.ts
+++ src/app/page/dwelling/detail/management/management.component.ts
@@ -1,16 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import {
-  DwellingCreate,
-  ManagerService,
-  DwellingService,
-} from '@availa/agube-rest-api';
+import { DwellingCreate, ManagerService } from '@availa/agube-rest-api';
 import { Type } from 'src/app/page/water-meter/detail/type';
 import { WaterMeterDialogData } from 'src/app/page/water-meter/dialog/dialog-data';
 import { WaterMeterDialogComponent } from 'src/app/page/water-meter/dialog/dialog.component';
-import { WaterMeterPersistantService } from 'src/app/page/water-meter/water-meter-persistant.service';
-import { WaterMeterType } from 'src/app/page/water-meter/water-meter-type.enum';
 import { ChangeData } from '../../manager/person/change/change-data';
 
 @Component({
@@ -18,60 +12,25 @@ import { ChangeData } from '../../manager/person/change/change-data';
   templateUrl: './management.component.html',
   styleUrls: ['./management.component.scss'],
 })
-export class ManagementComponent implements OnInit {
+export class ManagementComponent {
   @Input() public dwelling: DwellingCreate | undefined;
+  @Input() public waterMeterId: number | undefined;
+  @Input() public type: Type | undefined;
+  @Input() public load: boolean = false;
 
-  public load: boolean = false;
-
-  public waterMeterId: number | undefined;
-
-  public type: Type | undefined = undefined;
-
-  constructor(
-    private router: Router,
-    private svcManager: ManagerService,
-    private dialog: MatDialog,
-    private svcDwelling: DwellingService,
-    private svcPersistant: WaterMeterPersistantService
-  ) {
-    this.svcManager
-      .userIsManager()
-      .subscribe((response) => (this.load = response.is_manager));
-  }
-
-  ngOnInit(): void {
-    if (!this.dwelling) {
-      throw new Error('dwelling input is required');
-    }
-
-    this.svcDwelling
-      .getCurrentDwellingWaterMeter(this.dwelling.id!)
-      .subscribe((response) => {
-        this.waterMeterId = response.id;
-        this.svcPersistant.emitCode(response.code);
-      });
-
-    this.type = {
-      id: this.dwelling?.id!,
-      type: WaterMeterType.DWELLING,
-    };
-  }
+  constructor(private router: Router, private dialog: MatDialog) {}
 
   public openChangeWaterMeter() {
     let data: WaterMeterDialogData = {
       id: this.dwelling?.id!,
       type: this.type?.type!,
     };
-    const dialogRef = this.dialog.open(WaterMeterDialogComponent, {
+    this.dialog.open(WaterMeterDialogComponent, {
       hasBackdrop: true,
       width: '600px',
       disableClose: true,
       data,
     });
-
-    // dialogRef.afterClosed().subscribe(() => {
-    //   this.waterMeterChangeEvent.emit(true);
-    // });
   }
 
   public goToChangeResident() {
