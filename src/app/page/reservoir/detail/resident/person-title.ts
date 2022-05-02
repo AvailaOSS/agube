acdff846f6151e284500f362e845148fdab461b9
@germangaristo1989 <germangaristo1989@gmail.com>
2022-05-02 16:21:53 +0200

    feat: added new page detail module

---
+++ src/app/page/reservoir/detail/resident/person-title.ts
@@ -0,0 +1,4 @@
+export interface PersonTitle {
+  title: string;
+  icon: string;
+}
---
+++ src/app/page/reservoir/detail/resident/resident.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  UserService,
+  UserDetail,
+  DwellingService,
+} from '@availa/agube-rest-api';
+import { PersonTitle } from './person-title';
+
+@Component({
+  selector: 'app-resident',
+  templateUrl: './resident.component.html',
+  styleUrls: ['../info.component.scss'],
+})
+export class ResidentComponent implements OnInit {
+  @Input() public dwellingId: number | undefined;
+
+  public title: PersonTitle = {
+    title: 'GENERAL.TEXT.RESIDENT',
+    icon: 'escalator_warning',
+  };
+  public userDetail: UserDetail | undefined;
+
+  constructor(
+    protected svcUser: UserService,
+    protected svcDwelling: DwellingService
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.dwellingId) {
+      return;
+    }
+    this.svcDwelling
+      .getCurrentResident(this.dwellingId)
+      .subscribe((response) => {
+        if (!response.user.id) {
+          return;
+        }
+        this.getUser(response.user.id);
+      });
+  }
+
+  public getUser(userId: number) {
+    this.svcUser.getUserDetail(userId).subscribe((response) => {
+      this.userDetail = response;
+    });
+  }
+}
