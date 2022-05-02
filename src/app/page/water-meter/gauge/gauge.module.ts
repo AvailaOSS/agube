9f0beb0b7f6e21a1e9e56e1305c25bd07fcbe8b5
Fran <ffrannabril@gmail.com>
2022-05-02 18:47:57 +0200

    feat: translation

--- src/app/page/water-meter/gauge/gauge.module.ts
+++ src/app/page/water-meter/gauge/gauge.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GaugeComponent } from './gauge.component';
 import { ChartModule } from 'src/app/components/chart/chart.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [GaugeComponent],
-  imports: [CommonModule, ChartModule],
+  imports: [CommonModule, ChartModule, TranslateModule],
   exports: [GaugeComponent],
 })
 export class GaugeModule {}
