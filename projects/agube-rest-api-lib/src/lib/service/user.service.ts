267f879229bf94e4e4f9e9801b6ad2dfffc40459
@germangaristo1989 <germangaristo1989@gmail.com>
2022-05-02 11:41:47 +0200

    fix: changued config id from string to number

--- projects/agube-rest-api-lib/src/lib/service/user.service.ts
+++ projects/agube-rest-api-lib/src/lib/service/user.service.ts
@@ -574,22 +574,22 @@ export class UserService {
    * @param reportProgress flag to report request and response progress.
    */
   public getConfig(
-    id: string,
+    id: number,
     observe?: 'body',
     reportProgress?: boolean
   ): Observable<UserDetailConfigure>;
   public getConfig(
-    id: string,
+    id: number,
     observe?: 'response',
     reportProgress?: boolean
   ): Observable<HttpResponse<UserDetailConfigure>>;
   public getConfig(
-    id: string,
+    id: number,
     observe?: 'events',
     reportProgress?: boolean
   ): Observable<HttpEvent<UserDetailConfigure>>;
   public getConfig(
-    id: string,
+    id: number,
     observe: any = 'body',
     reportProgress: boolean = false
   ): Observable<any> {
@@ -706,25 +706,25 @@ export class UserService {
    * @param reportProgress flag to report request and response progress.
    */
   public updateConfig(
-    id: string,
+    id: number,
     data: UserDetailConfigure,
     observe?: 'body',
     reportProgress?: boolean
   ): Observable<Array<UserDetailConfigure>>;
   public updateConfig(
-    id: string,
+    id: number,
     data: UserDetailConfigure,
     observe?: 'response',
     reportProgress?: boolean
   ): Observable<HttpResponse<Array<UserDetailConfigure>>>;
   public updateConfig(
-    id: string,
+    id: number,
     data: UserDetailConfigure,
     observe?: 'events',
     reportProgress?: boolean
   ): Observable<HttpEvent<Array<UserDetailConfigure>>>;
   public updateConfig(
-    id: string,
+    id: number,
     data: UserDetailConfigure,
     observe: any = 'body',
     reportProgress: boolean = false
@@ -874,61 +874,87 @@ export class UserService {
       }
     );
   }
- /**
-     *
-     * Return user information details.
-     * @param id
-     * @param data
-     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
-     * @param reportProgress flag to report request and response progress.
-     */
-  public updateUserDetail(id: number, data: UserDetail, observe?: 'body', reportProgress?: boolean): Observable<UserDetail>;
-  public updateUserDetail(id: number, data: UserDetail, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<UserDetail>>;
-  public updateUserDetail(id: number, data: UserDetail, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<UserDetail>>;
-  public updateUserDetail(id: number, data: UserDetail, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
-
-      if (id === null || id === undefined) {
-          throw new Error('Required parameter id was null or undefined when calling updateUserDetail.');
-      }
-
-      if (data === null || data === undefined) {
-          throw new Error('Required parameter data was null or undefined when calling updateUserDetail.');
-      }
-
-      let headers = this.defaultHeaders;
-
-      // authentication (Basic) required
-      if (this.configuration.username || this.configuration.password) {
-          headers = headers.set('Authorization', 'Basic ' + btoa(this.configuration.username + ':' + this.configuration.password));
-      }
-
-      // to determine the Accept header
-      let httpHeaderAccepts: string[] = [
-          'application/json'
-      ];
-      const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
-      if (httpHeaderAcceptSelected != undefined) {
-          headers = headers.set('Accept', httpHeaderAcceptSelected);
-      }
-
-      // to determine the Content-Type header
-      const consumes: string[] = [
-          'application/json'
-      ];
-      const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
-      if (httpContentTypeSelected != undefined) {
-          headers = headers.set('Content-Type', httpContentTypeSelected);
-      }
-
-      return this.httpClient.put<Array<UserDetail>>(`${this.basePath}/user/${encodeURIComponent(String(id))}/`,
-          data,
-          {
-              withCredentials: this.configuration.withCredentials,
-              headers: headers,
-              observe: observe,
-              reportProgress: reportProgress
-          }
+  /**
+   *
+   * Return user information details.
+   * @param id
+   * @param data
+   * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+   * @param reportProgress flag to report request and response progress.
+   */
+  public updateUserDetail(
+    id: number,
+    data: UserDetail,
+    observe?: 'body',
+    reportProgress?: boolean
+  ): Observable<UserDetail>;
+  public updateUserDetail(
+    id: number,
+    data: UserDetail,
+    observe?: 'response',
+    reportProgress?: boolean
+  ): Observable<HttpResponse<UserDetail>>;
+  public updateUserDetail(
+    id: number,
+    data: UserDetail,
+    observe?: 'events',
+    reportProgress?: boolean
+  ): Observable<HttpEvent<UserDetail>>;
+  public updateUserDetail(
+    id: number,
+    data: UserDetail,
+    observe: any = 'body',
+    reportProgress: boolean = false
+  ): Observable<any> {
+    if (id === null || id === undefined) {
+      throw new Error(
+        'Required parameter id was null or undefined when calling updateUserDetail.'
       );
+    }
+
+    if (data === null || data === undefined) {
+      throw new Error(
+        'Required parameter data was null or undefined when calling updateUserDetail.'
+      );
+    }
+
+    let headers = this.defaultHeaders;
+
+    // authentication (Basic) required
+    if (this.configuration.username || this.configuration.password) {
+      headers = headers.set(
+        'Authorization',
+        'Basic ' +
+          btoa(this.configuration.username + ':' + this.configuration.password)
+      );
+    }
+
+    // to determine the Accept header
+    let httpHeaderAccepts: string[] = ['application/json'];
+    const httpHeaderAcceptSelected: string | undefined =
+      this.configuration.selectHeaderAccept(httpHeaderAccepts);
+    if (httpHeaderAcceptSelected != undefined) {
+      headers = headers.set('Accept', httpHeaderAcceptSelected);
+    }
+
+    // to determine the Content-Type header
+    const consumes: string[] = ['application/json'];
+    const httpContentTypeSelected: string | undefined =
+      this.configuration.selectHeaderContentType(consumes);
+    if (httpContentTypeSelected != undefined) {
+      headers = headers.set('Content-Type', httpContentTypeSelected);
+    }
+
+    return this.httpClient.put<Array<UserDetail>>(
+      `${this.basePath}/user/${encodeURIComponent(String(id))}/`,
+      data,
+      {
+        withCredentials: this.configuration.withCredentials,
+        headers: headers,
+        observe: observe,
+        reportProgress: reportProgress,
+      }
+    );
   }
 
   /**
