d9d105696e3550d994bfa72c4b62a281e021994f
@germangaristo1989 <germangaristo1989@gmail.com>
2022-05-02 13:59:19 +0200

    fix:  assigned new phone to manager create

--- manager/tasks.py
+++ manager/tasks.py
@@ -6,7 +6,8 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from manager.models import Manager, Person
 from userconfig.models import UserConfig
-
+from phone.models import Phone
+from login.models import UserPhone
 
 @task(autoretry_for=(ObjectDoesNotExist, ),
       retry_kwargs={
@@ -21,6 +22,11 @@ def new_user_published(data):
     # TODO: check json is valid
     json_response = json.loads(data)
     user = User.objects.get(id=json_response["id"])
+    # Create UserPhone
+    new_phone = Phone.objects.create(
+        phone_number=json_response["phone_number"])
+    UserPhone.objects.create(user=user, phone=new_phone, main=True)
+    # Create Manager
     manager = Manager.objects.create(user=user)
     # Important: create Person after create User
     person = Person.objects.create(manager=manager, user=user)
