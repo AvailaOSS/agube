5b5901ba65e27ef434d4225e51ab8fc9a2628823
@germangaristo1989 <germangaristo1989@gmail.com>
2022-05-05 20:51:26 +0200

    wip : added module contact in manager

--- src/app/page/config/manager/manager-page.module.ts
+++ src/app/page/config/manager/manager-page.module.ts
@@ -14,6 +14,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ContactBookModule } from '@availa/contact-book-fe';
 
 @NgModule({
   declarations: [ManagerPageComponent, ParametersComponent],
@@ -31,7 +32,8 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     MatTooltipModule,
     MatProgressBarModule,
     TranslateModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    ContactBookModule
   ],
 })
 export class ManagerPageModule {}
