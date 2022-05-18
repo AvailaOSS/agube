41b1bd1e59bc0bf0c81db6b13a3b3cb6ebd7d155
@germangaristo1989 <germangaristo1989@gmail.com>
2022-05-18 20:28:13 +0200

    fix: clean code up

--- src/app/components/upload-images/upload-images.component.ts
+++ src/app/components/upload-images/upload-images.component.ts
@@ -3,7 +3,7 @@ import { NotificationService } from '@availa/notification';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FileUploadService } from './service/file-upload.service';
-import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'app-upload-images',
@@ -12,33 +12,24 @@ import { HttpEventType, HttpResponse } from '@angular/common/http';
 })
 export class UploadImagesComponent implements OnInit {
     selectedFiles?: FileList;
-    selectedFileNames: string[] = [];
     previews: string[] = [];
     imageInfos?: Observable<any>;
     public fileInput = new FormControl('');
     constructor(private uploadService: FileUploadService, private svcNotification: NotificationService) {}
     selectFiles(event: any): void {
-        this.selectedFileNames = [];
         this.selectedFiles = event.target.files;
-        this.previews = [];
-        if (this.selectedFiles && this.selectedFiles[0]) {
-            const numberOfFiles = this.selectedFiles.length;
-            for (let i = 0; i < numberOfFiles; i++) {
-                const reader = new FileReader();
-                reader.onload = (e: any) => {
-                    console.log(e.target.result);
-                    this.previews.push(e.target.result);
-                };
-                reader.readAsDataURL(this.selectedFiles[i]);
-                this.selectedFileNames.push(this.selectedFiles[i].name);
-            }
+        if (this.selectedFiles) {
+            const reader = new FileReader();
+            reader.onload = (e: any) => {
+                console.log(e.target.result);
+                this.previews.push(e.target.result);
+                reader.readAsDataURL(this.selectedFiles![0]);
+            };
         }
     }
     uploadFiles(): void {
         if (this.selectedFiles) {
-            for (let i = 0; i < this.selectedFiles.length; i++) {
-                this.upload(i, this.selectedFiles[i]);
-            }
+            this.upload(0, this.selectedFiles![0]);
         }
     }
     upload(idx: number, file: File): void {
