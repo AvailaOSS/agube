9d70b1ef5f8ab11288c13c086d003fd83d32ffd4
Santi <sbl93.it@gmail.com>
2022-05-18 17:42:40 +0200

    refactor: urls

--- owner/urls.py
+++ owner/urls.py
@@ -3,8 +3,10 @@ from django.urls import include, path
 from owner.views import OwnerListView, OwnerView
 
 __url_owner = [
-    path('', OwnerListView.as_view()),
-    path('/<int:pk>', OwnerView.as_view()),
+    path('<int:pk>', OwnerView.as_view()),
 ]
 
-urlpatterns = [path('', include(__url_owner))]
+urlpatterns = [
+    path('', OwnerListView.as_view()),
+    path('/', include(__url_owner))
+]
--- resident/urls.py
+++ resident/urls.py
@@ -3,8 +3,10 @@ from django.urls import include, path
 from resident.views import ResidentListView, ResidentView
 
 __url_resident = [
-    path('', ResidentListView.as_view()),
-    path('/<int:pk>', ResidentView.as_view()),
+    path('<int:pk>', ResidentView.as_view()),
 ]
 
-urlpatterns = [path('', include(__url_resident))]
+urlpatterns = [
+    path('', ResidentListView.as_view()),
+    path('/', include(__url_resident))
+]
