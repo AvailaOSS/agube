762a4e1391e7d95175e3d3eeaf96aba9e93eb972
@germangaristo1989 <germangaristo1989@gmail.com>
2022-05-18 10:30:31 +0200

    fix: permission geolocation in reservoir

--- geolocation/permissions.py
+++ geolocation/permissions.py
@@ -2,41 +2,48 @@ from manager.permissions import IsManagerAuthenticated
 
 from geolocation.models import Geolocation
 from dwelling.models import Dwelling
-from reservoir.models import ReservoirOwner, Reservoir
+from reservoir.models import ReservoirOwner
 from person.models import Person
 from user.models import UserGeolocation
+from django.core.exceptions import ObjectDoesNotExist
+
 
 class IsManagerOfGeolocation(IsManagerAuthenticated):
     """
     Allows access only to authenticated managers of the community geolocation.
     """
     message = 'The authenticated user has to be the Manager of the community geolocation to execute this operation.'
+
     def has_permission(self, request, view):
         if (not super().has_permission(request, view)):
             return False
 
         try:
             geolocation = Geolocation.objects.get(id=view.kwargs.get('pk'))
-        except:
+        except ObjectDoesNotExist:
             return False
 
         # Dwelling
         try:
             dwelling = Dwelling.objects.get(manager__user=request.user,
-            geolocation=geolocation)
+                                            geolocation=geolocation)
             return True
-        except: pass
+        except ObjectDoesNotExist:
+            pass
         # Reservoir
         try:
-            reservoir_owner = ReservoirOwner.objects.get(user=request.user,
-                reservoir__geolocation=geolocation)
-            return true
-        except: pass
+            reservoir_owner = ReservoirOwner.objects.get(
+                user=request.user, reservoir__geolocation=geolocation)
+            return True
+        except ObjectDoesNotExist:
+            pass
         # User
         try:
-            person = Person.objects.get(manager__user=request.user,
+            person = Person.objects.get(
+                manager__user=request.user,
                 user=UserGeolocation.objects.get(geolocation=geolocation).user)
             return True
-        except: pass
+        except ObjectDoesNotExist:
+            pass
 
         return False
