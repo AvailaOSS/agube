79db20c8ef9f326208026c27262daef45ed49137
@germangaristo1989 <germangaristo1989@gmail.com>
2022-05-19 18:22:30 +0200

    fix: update version and translate

--- src/app/page/home/sidebar.component.ts
+++ src/app/page/home/sidebar.component.ts
@@ -1,15 +1,14 @@
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { UserDetail, UserService } from '@availa/agube-rest-api';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component, HostBinding } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { PersonConfig, UserDetail, UserService } from '@availa/agube-rest-api';
 import { AccountService } from '@availa/auth-fe';
+import { NotificationService } from '@availa/notification';
+import { environment } from 'src/environments/environment';
 import { SidebarConfig } from './sidebar-config';
 import { ThemeMode } from './theme-mode';
-import { PersonConfig } from '@availa/agube-rest-api';
-import { NotificationService } from '@availa/notification';
+
 @Component({
     selector: 'app-sidebar',
     template: `<ng-template></ng-template>`,
@@ -34,7 +33,6 @@ export class SidebarComponent {
         protected readonly accountService: AccountService,
         protected overlayContainer: OverlayContainer,
         private svcUser: UserService,
-        private httpClient: HttpClient,
         private svcNotification: NotificationService
     ) {
         //FIXME: add pipe with first name and last name
@@ -47,19 +45,14 @@ export class SidebarComponent {
 
             const urlPath = `${environment.agubeBackendUrl}/user/${encodeURIComponent(String(this.userId))}/photo`;
 
-            this.httpClient
-                .get(urlPath, {
-                    observe: 'body',
-                    responseType: 'blob',
-                })
-                .subscribe({
-                    next: (response) => {
-                        const reader = new FileReader();
-                        reader.addEventListener('load', () => (this.profilePhoto = reader.result), false);
-                        reader.readAsDataURL(response);
-                    },
-                    error: (error) => this.svcNotification.warning({ message: error.error }),
-                });
+            this.svcUser.getUserPhoto(this.userId).subscribe({
+                next: (response) => {
+                    const reader = new FileReader();
+                    reader.addEventListener('load', () => (this.profilePhoto = reader.result), false);
+                    reader.readAsDataURL(response);
+                },
+                error: (error) => this.svcNotification.warning({ message: error.error }),
+            });
 
             this.toggleControl.valueChanges.subscribe((isDarkMode) => {
                 if (isDarkMode) {
