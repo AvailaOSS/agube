0b2947128c477ae84ba21e34d9f3b148a8d71c5f
Fran <ffrannabril@gmail.com>
2022-08-11 15:17:33 +0200

    fix: water meter code can be optional

--- src/app/page/dwelling/card-button/card-button.component.ts
+++ src/app/page/dwelling/card-button/card-button.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserDwellingDetail } from '@availa/agube-rest-api';
-import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
 @Component({
     selector: 'app-card-button',
