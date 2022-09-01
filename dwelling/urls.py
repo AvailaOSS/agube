3e02e289d9f4c60725e259767805c12abf8ba8d5
Santi <sbl93.it@gmail.com>
2022-09-01 15:53:08 +0200

    feat: dwelling watermeter measurements pagination

--- dwelling/urls.py
+++ dwelling/urls.py
@@ -5,21 +5,24 @@ from dwelling.views import (DwellingResumeView, DwellingCreateView, DwellingList
                             DwellingSetOwnerAsResidentView, DwellingView,
                             DwellingWaterMeterChunkView,
                             DwellingWaterMeterHistoricalView,
-                            DwellingWaterMeterView)
+                            DwellingWaterMeterView,
+                            DwellingWaterMeterMeasurementsView)
 
 __url_dwelling = [
-    path('resume', DwellingResumeView.as_view()),
-    path('create', DwellingCreateView.as_view()),
-    path('<int:pk>', DwellingView.as_view()),
-    path('<int:pk>/owner', DwellingOwnerView.as_view()),
-    path('<int:pk>/owner-as-resident',
-         DwellingSetOwnerAsResidentView.as_view()),
-    path('<int:pk>/resident', DwellingResidentView.as_view()),
-    path('<int:pk>/water-meter', DwellingWaterMeterView.as_view()),
-    path('<int:pk>/water-meter/historical',
-         DwellingWaterMeterHistoricalView.as_view()),
-    path('<int:pk>/water-meter/<int:chunk>',
-         DwellingWaterMeterChunkView.as_view())
+     path('resume', DwellingResumeView.as_view()),
+     path('create', DwellingCreateView.as_view()),
+     path('<int:pk>', DwellingView.as_view()),
+     path('<int:pk>/owner', DwellingOwnerView.as_view()),
+     path('<int:pk>/owner-as-resident',
+          DwellingSetOwnerAsResidentView.as_view()),
+     path('<int:pk>/resident', DwellingResidentView.as_view()),
+     path('<int:pk>/water-meter', DwellingWaterMeterView.as_view()),
+     path('<int:pk>/water-meter/historical',
+          DwellingWaterMeterHistoricalView.as_view()),
+     path('<int:pk>/water-meter/<int:chunk>',
+          DwellingWaterMeterChunkView.as_view()),
+     path('<int:pk>/water-meter/masurements/',
+          DwellingWaterMeterMeasurementsView.as_view())
 ]
 
 urlpatterns = [
