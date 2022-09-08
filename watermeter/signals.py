bb68c4c485cc4db397176abf1d3c79ee85647142
Fran <ffrannabril@gmail.com>
2022-09-08 11:07:32 +0200

    feat: send email on measure edited

--- watermeter/signals.py
+++ watermeter/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.core.exceptions import ObjectDoesNotExist
 from manager.models import ManagerConfiguration
 
-from watermeter.send import send_email_measurement, MeasurementEmailType
+from watermeter.send import MeasurementEditedEmailType, send_email_measurement, MeasurementEmailType
 from watermeter.models import WaterMeterMeasurement
 from dwelling.models import DwellingWaterMeter
 from resident.models import Resident
@@ -12,36 +12,45 @@ from watermeter.utils import is_24h_old_than_now
 
 @receiver(post_save, sender=WaterMeterMeasurement)
 def measure_update(sender, created, instance, **kwargs):
+    watermeter_measurement: WaterMeterMeasurement = WaterMeterMeasurement.objects.get(id=instance.id)
+
+    # Check if measurement is last and recent (24h)
+    if watermeter_measurement != watermeter_measurement.water_meter.get_last_measurement() or is_24h_old_than_now(watermeter_measurement.date):
+        return
+
+    # Check if watermeter is from a Dwelling
+    dwelling_water_meter: DwellingWaterMeter
+    try:
+        dwelling_water_meter = DwellingWaterMeter.objects.get(
+            water_meter=watermeter_measurement.water_meter)
+    except ObjectDoesNotExist:
+        return
+
+    # Check if dwelling has a resident
+    resident: Resident = dwelling_water_meter.dwelling.get_current_resident()
+    if (resident == None):
+        return
+
+    manager_configuration: ManagerConfiguration = dwelling_water_meter.dwelling.manager.get_closest_config(watermeter_measurement.date)
+
     if created:
-        watermeter_measurement: WaterMeterMeasurement = WaterMeterMeasurement.objects.get(
-            id=instance.id)
-
-        # Check if measurement is last and recent (24h)
-        if watermeter_measurement != watermeter_measurement.water_meter.get_last_measurement() or is_24h_old_than_now( watermeter_measurement.date):
-            return
-
-        # Check if watermeter is from a Dwelling
-        dwelling_water_meter: DwellingWaterMeter
-        try:
-            dwelling_water_meter = DwellingWaterMeter.objects.get(
-                water_meter=watermeter_measurement.water_meter)
-        except ObjectDoesNotExist:
-            return
-
-        # Check if dwelling has a resident
-        resident: Resident = dwelling_water_meter.dwelling.get_current_resident(
-        )
-        if (resident == None):
-            return
-
-        manager_configuration: ManagerConfiguration = dwelling_water_meter.dwelling.manager.get_closest_config(watermeter_measurement.date)
-
         # Select template
-        if (watermeter_measurement.measurement_diff >
-                manager_configuration.max_daily_consumption):
+        if (watermeter_measurement.measurement_diff > manager_configuration.max_daily_consumption):
             email_template = MeasurementEmailType.EXCESIVE_MEASUREMENT_EMAIL.value
         else:
             email_template = MeasurementEmailType.CORRECT_MEASUREMENT_EMAIL.value
+
+        # Send measurement notification email
+        send_email_measurement(user=resident.user,
+                               watermeter_measurement=watermeter_measurement,
+                               manager_configuration=manager_configuration,
+                               email_template=email_template)
+    else:
+        # Select template
+        if (watermeter_measurement.measurement_diff > manager_configuration.max_daily_consumption):
+            email_template = MeasurementEditedEmailType.EXCESIVE_MEASUREMENT_EMAIL.value
+        else:
+            email_template = MeasurementEditedEmailType.CORRECT_MEASUREMENT_EMAIL.value
 
         # Send measurement notification email
         send_email_measurement(user=resident.user,
