733aedfd95e50a07c2dccda204d02e89a41fc3a5
@germangaristo1989 <germangaristo1989@gmail.com>
2022-09-15 13:55:26 +0200

    feat: update api and implemented new component notification

--- src/app/page/config/manager/configuration-email/configuration-email.component.ts
+++ src/app/page/config/manager/configuration-email/configuration-email.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { ManagerService } from '@availa/agube-rest-api';
+import { NotificationService } from '@availa/notification';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-configuration-email',
@@ -8,13 +11,54 @@ import { FormControl } from '@angular/forms';
 })
 export class ConfigurationEmailComponent implements OnInit {
     public loadSave: boolean = false;
-    public message = new FormControl('', []);
+    public message = new FormControl('', [Validators.required]);
 
-    constructor() {}
+    constructor(
+        private svcManager: ManagerService,
+        private svcNotification: NotificationService,
+        private svcTranslate: TranslateService
+    ) {}
 
-    ngOnInit(): void {}
+    public errorValidator(entity: string) {
+        switch (entity) {
+            case 'message':
+                if (this.message.hasError('required')) {
+                    return 'PAGE.CONFIG.MANAGER.NOTIFICATION.MESSAGE_REQUIRED';
+                }
+                return '';
+            default:
+                return '';
+        }
+    }
+    public ngOnInit(): void {
+        this.svcManager.getManagerMessage().subscribe((res) => {
+            console.log(res);
+            this.message.setValue(res.message);
+        });
+    }
 
-    saveNotification() {
+    public saveNotification() {
         this.loadSave = true;
+        let data: any = {
+            is_active: true,
+            message: this.message.value,
+        };
+        this.svcManager.updateManagerMessage(data).subscribe({
+            next: () => {
+                this.loadSave = false;
+                this.ngOnInit();
+            },
+            error: (error) => {
+                this.loadSave = false;
+                let message = JSON.stringify(error.error);
+
+                if (error.status === 404) {
+                    this.svcTranslate
+                        .get('PAGE.CONFIG.MANAGER.NOTIFICATION.MESSAGE_ERROR')
+                        .subscribe((response) => (message = response));
+                }
+                this.svcNotification.warning({ message: message });
+            },
+        });
     }
 }
