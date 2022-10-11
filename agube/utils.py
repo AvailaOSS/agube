cca5ba61e24b24e2751951180c432f44654eb624
Santi <sbl93.it@gmail.com>
2022-10-11 10:32:17 +0200

    feat: timezone support for datetime utils

--- agube/utils.py
+++ agube/utils.py
@@ -1,29 +1,41 @@
 from datetime import datetime, timedelta, date, time
 import pytz
 from typing import Tuple
-from django.utils import dateparse, timezone
+from django.utils import dateparse
 from agube.exceptions import DateFilterNoEndDateError, DateFilterBadFormatError, DateFilterStartGtEnd
 
 
 def is_24h_older_than_now(datetime):
     # type: (datetime) -> bool
     utc_tz = pytz.timezone('UTC')
-    return (datetime.now(utc_tz) - datetime.astimezone(utc_tz)) >= timedelta(hours=24)
+    return (datetime.now(utc_tz) -
+            datetime.astimezone(utc_tz)) >= timedelta(hours=24)
 
 
-def parse_query_datetime(var) -> datetime:
-    # type: (date | str) -> datetime
+def parse_query_datetime(var, timezone=None) -> datetime:
+    # type: (date | str, pytz.BaseTzInfo) -> datetime
     if var is None:
         return None
     if isinstance(var, datetime):
-        return var
+        return __make_aware(var, timezone)
     if isinstance(var, date):
-        return __date_to_datetime(var)
+        return __make_aware(__date_to_datetime(var), timezone)
+
+    # parse from str
     parsed_datetime = dateparse.parse_datetime(var)
-    # try date to datetime
     if parsed_datetime is None:
-        return __date_to_datetime(dateparse.parse_date(var))
-    return parsed_datetime
+        # try date to datetime
+        parsed_date = dateparse.parse_date(var)
+        if parsed_date is None:
+            return None
+        parsed_datetime = __date_to_datetime(parsed_date)
+    return __make_aware(parsed_datetime, timezone)
+
+
+def __make_aware(datetime: datetime, timezone: pytz.BaseTzInfo) -> datetime:
+    if datetime.tzinfo is None and timezone != None:
+        return timezone.localize(datetime)
+    return datetime
 
 
 def parse_query_date(var) -> date:
@@ -35,15 +47,14 @@ def parse_query_date(var) -> date:
     return dateparse.parse_date(var)
 
 
-def __date_to_datetime(var) -> datetime:
-    # type: (date) -> datetime
-    return datetime.combine(var, time(),
-                            timezone.get_current_timezone())
+def __date_to_datetime(date: date) -> datetime:
+    return datetime.combine(date, time())
 
 
 def validate_query_date_filters(
         query_start_date: str,
-        query_end_date: str) -> Tuple[datetime, datetime]:
+        query_end_date: str,
+        timezone: pytz.BaseTzInfo = None) -> Tuple[datetime, datetime]:
 
     if (query_start_date is None) and (query_end_date is None):
         return None
@@ -52,8 +63,8 @@ def validate_query_date_filters(
         raise DateFilterNoEndDateError
 
     # Parse to datetime
-    start_datetime = parse_query_datetime(query_start_date)
-    end_datetime = parse_query_datetime(query_end_date)
+    start_datetime = parse_query_datetime(query_start_date, timezone)
+    end_datetime = parse_query_datetime(query_end_date, timezone)
 
     # If after parse is None, bad format
     if (start_datetime is None
@@ -78,4 +89,4 @@ def validate_datetime_filters(
 
 
 def timedelta_in_days(timedelta: timedelta):
-    return timedelta.total_seconds()/86400
+    return timedelta.total_seconds() / 86400
