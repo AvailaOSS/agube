5eb11a8ecc96a329ebd3cfe6739889fa238a9012
Santiago Bolás Lago <sbl93.it@gmail.com>
2022-10-25 13:04:09 +0200

    feat: manager creation command

--- server/agube/tasks.py
+++ server/agube/tasks.py
@@ -1,4 +1,3 @@
-import json
 from django.contrib.auth.models import User
 from django.db import transaction
 
@@ -8,16 +7,12 @@ from person.models import PersonConfig
 from phone.models import Phone
 from user.models import UserPhone
 
-
-def new_user_published(data):
-    # TODO: check json is valid
-    json_response = json.loads(data)
+def create_manager(user: User, phone_number: str = None):
     with transaction.atomic():
-        user = User.objects.get(id=json_response["id"])
-        # Create UserPhone
-        new_phone = Phone.objects.create(
-            phone_number=json_response["phone_number"])
-        UserPhone.objects.create(user=user, phone=new_phone, main=True)
+        if phone_number:
+            # Create UserPhone
+            new_phone = Phone.objects.create(phone_number=phone_number)
+            UserPhone.objects.create(user=user, phone=new_phone, main=True)
         # Create Manager
         manager = Manager.objects.create(user=user)
         # Important: create Person after create User
---
+++ server/manager/management/commands/createmanager.py
@@ -0,0 +1,63 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.contrib.auth.password_validation import validate_password
+
+from agube.tasks import create_manager
+from getpass import getpass
+
+
+class Command(BaseCommand):
+    help = 'Create a manager user'
+
+    def add_arguments(self, parser) -> None:
+        parser.add_argument('username', type=str)
+        parser.add_argument('email', type=str)
+        parser.add_argument('first_name', type=str)
+        parser.add_argument('last_name', type=str)
+        parser.add_argument('phone_number', type=str)
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+
+        # Check username already exists
+        try:
+            user = User.objects.get(username=options['username'])
+            self.stderr.write('Error: Username "%s" already exists' % user.username)
+            return
+        except ObjectDoesNotExist:
+            pass
+
+        # Password prompt
+        password = getpass()
+        password2 = getpass('Password (again): ')
+        # Password validation (django password validation)
+        if password != password2:
+            self.stderr.write("Error: Your passwords didn't match.")
+            return
+        if password.strip() == '':
+            self.stderr.write("Error: Blank passwords aren't allowed.")
+            return
+        try:
+            validate_password(password2)
+        except ValidationError as err:
+            self.stderr.write('\n'.join(err.messages))
+            response = input(
+                'Bypass password validation and create user anyway? [y/N]: ')
+            if response.lower() != 'y':
+                return
+
+        # Create user
+        user = User.objects.create(username=options['username'],
+                                   email=options['email'],
+                                   first_name=options['first_name'],
+                                   last_name=options['last_name'])
+        user.set_password(password2)
+        user.save()
+
+        # Create manager from user
+        create_manager(user, options['phone_number'])
+
+        self.stdout.write(
+            self.style.SUCCESS('Succesfully created manager "%s"' %
+                               user.username))
